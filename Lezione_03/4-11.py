8cc528da6baed9c134612fcb12e0efcc6e4e94d0
alessandroricci-itsictacademy <146222580+alessandroricci-itsictacademy@users.noreply.github.com>
2025-02-26 17:35:46 +0100

    Add files via upload

--- Lezione_03/4-11.py
+++ Lezione_03/4-11.py
@@ -3,3 +3,18 @@
 • Add a different pizza to the list friend_pizzas.
 • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list. """
 
+favorite_pizzas = ["Diavola", "Margherita", "Cacio & pepe", "4 Formaggi", "Broccoli & salsiccia"]
+
+friend_pizzas = favorite_pizzas[:]
+
+favorite_pizzas.append("Mortadella & pistacchio")
+
+friend_pizzas.append("Marinara")
+
+print("My favorite pizzas are:")
+for pizza in favorite_pizzas:
+    print(pizza)
+
+print("My friend’s favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
